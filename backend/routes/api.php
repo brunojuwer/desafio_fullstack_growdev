3c1e8897a7745d5054eeba058187f8ed3806b236
Bruno Juwer <bruno.juwer1997@gmail.com>
2024-09-03 21:37:38 -0300

    chore: correct frontend folder name

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MentorController;
 use Illuminate\Support\Facades\Route;
 
-sleep(env('DELAY_TIME', 0));
-
 Route::post('/login', [AuthController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,6 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
   <RouterView />
 </template>
 
